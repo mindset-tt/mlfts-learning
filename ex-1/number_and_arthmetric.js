1669dcadb7a50a448f5b535c5a7f58652a84047d
mindset-tt <205n001419@nuol.edu.la>
2023-09-13 15:37:31 +0700

    remove unnessaries thing

--- ex-1/number_and_arthmetric.js
+++
@@ -1,8 +0,0 @@
-export const dayRate = (ratePerHour) => ratePerHour * 8;
-export const daysInBudget = (budget, ratePerHour) =>
-  Math.floor(budget / (ratePerHour * 8));
-export const priceWithMonthlyDiscount = (ratePerHour, numDays, discount) =>
-  Math.ceil(
-    Math.floor(numDays / 22) * 176 * ratePerHour * (1 - discount) +
-      (numDays % 22) * 8 * ratePerHour
-  );
