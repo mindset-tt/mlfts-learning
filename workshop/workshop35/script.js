b1350d8efece0c113463ab9466dc48f0965b71e6
mindset-tt <205n001419@nuol.edu.la>
2023-09-15 10:32:14 +0700

    update-error-code

--- workshop/workshop35/script.js
+++ workshop/workshop35/script.js
@@ -3,36 +3,44 @@ $(document).ready(function () {
     const resetBtn = $('#reset');
     const body = $('body');
     var text = $('p');
-    loadTheme();
-
-    //change theme from checkbox
+  
+    // change theme from checkbox
     setting.on('change', function () {
-        localStorage.setItem('night-mode', $(this).prop('checked'));
-        loadTheme();
+        //set status to local storage
+      localStorage.setItem('night-mode', $(this).prop('checked'));
+      //load theme
+      loadTheme();
     });
-
-    //set and load theme function
+  
+    // set and load theme function
     function loadTheme() {
-        var status = localStorage.getItem('night-mode');
-        setting.prop('checked', status);
-        if ($('#setting').prop('checked')) {
-            text.text('ໂຫມດມືດ');
-            body.css('background-color', 'black');
-            body.css('color', 'white');
-            body.css('transition', 'all 1s ease-in-out');
-        } else {
-            text.text('ໂຫມດແຈ້ງ');
-            body.css('background-color', 'white');
-            body.css('color', 'black');
-            body.css('transition', 'all 1s ease-in-out');
-        }
+        //get status from local storage
+      var status = localStorage.getItem('night-mode');
+      //check if status is true
+      if (status === 'true') {
+        //set theme to dark
+        text.text('ໂຫມດມືດ');
+        body.css('background-color', 'black');
+        body.css('color', 'white');
+        body.css('transition', 'all 1s ease-in-out');
+      } else {
+        //set theme to light
+        text.text('ໂຫມດແຈ້ງ');
+        body.css('background-color', 'white');
+        body.css('color', 'black');
+        body.css('transition', 'all 1s ease-in-out');
+      }
     }
-
-    //reset theme to default
+  
+    // reset theme to default
     resetBtn.on('click', function () {
-        localStorage.clear();
-        loadTheme();
+        //remove status from local storage
+      localStorage.removeItem('night-mode');
+      //set checkbox to false
+      setting.prop('checked', false);
+      //load theme
+      loadTheme();
     });
-
-
-});
+  
+    loadTheme();
+  });
